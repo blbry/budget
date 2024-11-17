f05ce28db0ede55dcd84b9970afb7ceabb85ff28
sn0rp <98631572+sn0rp@users.noreply.github.com>
2024-11-17 16:36:08 -0600

    skip properties page because I am a baby with no property

--- src/renderer/pages/assets/Properties.tsx
+++ src/renderer/pages/assets/Properties.tsx
@@ -4,6 +4,9 @@ function Properties() {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold">Properties</h1>
+      <br />
+      <h2>Coming Soon!</h2>
+      <p>(I have not yet dealt with this in real life and am thus not the person to build this out)</p>
     </div>
   );
 }
